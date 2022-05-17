fc35d1eb54d13b1166a51665740d62ad30834a3a
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2022-05-17 13:19:02 +0200

    Update 003-basic-image-manipulation-pil.py

--- 003-basic-image-manipulation-pil.py
+++ 003-basic-image-manipulation-pil.py
@@ -107,3 +107,79 @@ for key, values in flip.items():
     plt.imshow(image.transpose(values))
     plt.title(key)
     plt.show()
+
+#let us continue image manipulation with cropping
+#cropping using array slicing
+upper = 150
+lower = 400
+crop_top = array[upper: lower,:,:]
+plt.figure(figsize=(5,5))
+plt.imshow(crop_top)
+plt.show()
+
+#continue cropping
+left = 150
+right = 400
+crop_horizontal = crop_top[: ,left:right,:]
+plt.figure(figsize=(5,5))
+plt.imshow(crop_horizontal)
+plt.show()
+
+#we can crop using matplotlibs crop() method instead
+image = Image.open("cat.png")
+crop_image = image.crop((left, upper, right, lower))
+plt.figure(figsize=(5,5))
+plt.imshow(crop_image)
+plt.show()
+
+#let's flip the cropped image
+crop_image = crop_image.transpose(Image.FLIP_LEFT_RIGHT)
+crop_image
+
+#we can change individual pixels using array indexing, again
+array_sq = np.copy(array)
+array_sq[upper:lower, left:right, 1:2] = 0 #set all Green and Blue things to 0 in this area
+plt.figure(figsize=(5,5))
+plt.subplot(1,2,1)
+plt.imshow(array)
+plt.title("orignal")
+plt.subplot(1,2,2)
+plt.imshow(array_sq)
+plt.title("Altered Image")
+plt.show()
+
+#we can also use PILs ImageDraw module
+from PIL import ImageDraw 
+image_draw = image.copy()
+
+#The draw constructor creates an object that can be used to draw in the given image. The input im is the image we would like to draw in.
+image_fn = ImageDraw.Draw(im=image_draw)
+
+#Drawing a rectangle
+shape = [left, upper, right, lower] 
+image_fn.rectangle(xy=shape,fill="red")
+
+plt.figure(figsize=(10,10))
+plt.imshow(image_draw)
+plt.show()
+
+#Apart from rectangles, we can also add text
+from PIL import ImageFont
+image_fn.text(xy=(0,0),text="box",fill=(0,0,0))
+
+plt.figure(figsize=(10,10))
+plt.imshow(image_draw)
+plt.show()
+
+#overlaying one image over another
+image_lenna = Image.open("lenna.png")
+array_lenna = np.array(image_lenna)
+array_lenna[upper:lower,left:right,:]=array[upper:lower,left:right,:] #array is the cat picture data from above
+plt.imshow(array_lenna)
+plt.show()
+
+#instead of doing it with indexing, we can also use the paste method instead
+image_lenna.paste(crop_image, box=(left,upper))
+plt.imshow(image_lenna)
+plt.show()
+
