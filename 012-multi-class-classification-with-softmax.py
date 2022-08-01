c4992e546b7a37a4c9cf2c26740938cafbbaaa2b
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2022-08-01 17:57:16 +0200

    Update 012-multi-class-classification-with-softmax.py

--- 012-multi-class-classification-with-softmax.py
+++ 012-multi-class-classification-with-softmax.py
@@ -97,3 +97,57 @@ train_dataset[0][0].shape
 # Set input size and output size
 input_dim = 28 * 28
 output_dim = 10
+
+#DEFINITION SOFTMAX CLASSIFIER, CRITERION FUNCTION, OPTIMIZER + MODEL TRAINING
+# Create the model
+# Input dim is 28*28 which is the image converted to a tensor
+# Output dim is 10 because there are 10 possible digits the image can be
+model = SoftMax(input_dim, output_dim)
+print("Print the model:\n ", model)
+
+# Print the parameters
+print('W: ',list(model.parameters())[0].size())
+print('b: ',list(model.parameters())[1].size())
+
+# Plot the model parameters for each class
+# Since the model has not been trained yet the parameters look random
+PlotParameters(model)
+
+#Let us make a prediction
+# First we get the X value of the first image
+X = train_dataset[0][0]
+# We can see the shape is 1 by 28 by 28, we need it to be flattened to 1 by 28 * 28 (784)
+print(X.shape)
+X = X.view(-1, 28*28)
+print(X.shape)
+# Now we can make a prediction, each class has a value, and the higher it is the more confident the model is that it is that digit
+model(X)
+
+# Define the learning rate, optimizer, criterion, and data loader
+
+learning_rate = 0.1
+# The optimizer will updates the model parameters using the learning rate
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+# The criterion will measure the loss between the prediction and actual label values
+# This is where the SoftMax occurs, it is built into the Criterion Cross Entropy Loss
+criterion = nn.CrossEntropyLoss()
+# Created a training data loader so we can set the batch size
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=100)
+# Created a validation data loader so we can set the batch size
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=5000)
+
+###
+model_output = model(X)
+actual = torch.tensor([train_dataset[0][1]])
+show_data(train_dataset[0])
+print("Output: ", model_output)
+print("Actual:", actual)
+
+criterion(model_output, actual)
+softmax = nn.Softmax(dim=1)
+probability = softmax(model_output)
+print(probability)
+
+-1*torch.log(probability[0][actual])
+
+#TRAIN THE MODEL USING THE DATASETS
