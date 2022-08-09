ebfada9d2f8439bee2e72ab0b0350dbc2cfd16cb
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2022-08-09 10:31:56 +0200

    Update 021-cnn-image-classification-pytorch-and-transfer-learning-plus-ibm-computer-vision-learning-studio.py

--- 021-cnn-image-classification-pytorch-and-transfer-learning-plus-ibm-computer-vision-learning-studio.py
+++ 021-cnn-image-classification-pytorch-and-transfer-learning-plus-ibm-computer-vision-learning-studio.py
@@ -16,3 +16,59 @@
 #Usually, the more images you add, the better your results, but it takes longer and the rate of improvement will have decreasing marginal rate of return
 
 
+#IMPORTING LIBRARIES
+#Installation if not already here:
+#! conda install -c pytorch torchvision
+#! pip install skillsnetwork tqdm
+#!pip install  skillsnetwork
+
+#Libraries for accessing the operationg sytem of my (or cloud) machine, and IBM services
+import os
+import uuid
+import shutil
+import json
+from botocore.client import Config
+import ibm_boto3
+import copy
+from datetime import datetime
+from skillsnetwork import cvstudio 
+
+#Libraries for image and array manipulation, as well as data visualization
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+from matplotlib.pyplot import imshow
+from tqdm import tqdm
+from ipywidgets import IntProgress
+import time 
+
+#Libraries for machine learning
+import torch
+import torchvision.models as models
+from torch.utils.data import Dataset, DataLoader,random_split
+from torch.optim import lr_scheduler
+from torchvision import transforms
+import torch.nn as nn
+torch.manual_seed(0) #for reproducibility purposes, Im fixing the random seed manually
+
+#HELPER FUNCTIONS
+#fpr plotting train cost and validation accuracy
+def plot_stuff(COST,ACC):    
+    fig, ax1 = plt.subplots()
+    color = 'tab:red'
+    ax1.plot(COST, color = color)
+    ax1.set_xlabel('Iteration', color = color)
+    ax1.set_ylabel('total loss', color = color)
+    ax1.tick_params(axis = 'y', color = color)
+    
+    ax2 = ax1.twinx()  
+    color = 'tab:blue'
+    ax2.set_ylabel('accuracy', color = color)  # we already handled the x-label with ax1
+    ax2.plot(ACC, color = color)
+    ax2.tick_params(axis = 'y', color = color)
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    
+    plt.show()
+
