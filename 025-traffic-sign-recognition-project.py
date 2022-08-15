924ba4e0a5e6e3511d2c32ce71fc60b1af6d385c
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2022-08-15 13:05:53 +0200

    Create 025-traffic-sign-recognition-project.py

---
+++ 025-traffic-sign-recognition-project.py
@@ -0,0 +1,40 @@
+"""Project Overview
+In this course, you learned how to train and use custom classifiers. 
+For the project in this module, you will develop a new custom classifier using one of the classification methods 
+you learnt and then deploy it as a web app using Code Engine. 
+
+Project Scenario
+You have been employed as a Junior Data Scientist by Jokwu, a self-driving car start-up in Capetown, South Africa. 
+Jokwu has created the hardware and parts of the car, and they are beginning to create sensors; 
+the next step is to have a working model that identifies traffic signs. 
+The project and product team have decided to start with stop signs - is it a stop sign or not?
+
+As a first step, you have been given a dataset and tasked with training a model that identifies the stop signs in an image. 
+This will be integrated into a motion detector as a next step.
+
+Project Tasks
+Your job is to load the training images, 
+create features, 
+and train the model. 
+You will then deploy the model to Code Engine so your manager 
+can upload an image of a stop sign and your image classifier 
+will classify the image and tell them to what accuracy it predicts it is correct. 
+You will utilize CV Studio to develop this model and then deploy it as a web app by completing the tasks below.
+
+Once you are done, please take screenshots of your results to upload them for a peer evaluation. 
+Task 1: Gather and Upload Your Data
+Task 2: Train Your Classifier
+Task 3: Deploy Your Model
+Task 4: Test Your Classifier
+Task 5: Submit Your Assignment and Evaluate Your Peers"""
+
+#GATHERING AND UPLOADING DATA
+#Downloaded the images for stop sign and not stop sign
+#Uploaded them to IBM CV Studio
+#Ensured that all images are pre-labelled correctly (according to name of the folder)
+
+#TRAINING THE CLASSIFIER
+#Convolutional Neural Networks (CNN) with PyTorch.
+
+
+
